802e1fc9d3214f9a67600c8f110ff9c154651197
Papi <stevegoharder@gmail.com>
2024-10-21 16:21:43 +0200

    Spinning-wheel

---
+++ script.js
@@ -0,0 +1,16 @@
+const wheel = document.getElementById('wheel');
+let isHovered = false;
+wheel.addEventListener('mouseenter',()=>{
+    wheel.style.animationPlayState = 'paused'; isHovered= true;
+});
+wheel.addEventListener('mouseLeave',()=>{
+    wheel.style.animationPlaystate='running'; isHovered = false;
+});
+window.addEventListener('mousemove',(e)=>{
+    if (isHovered){ const x = e.clientX / window.innerWidth;
+        const y = e.clientY /window.
+        innerHeight;
+        const hue = Math.floor((x + y)* 180);
+        wheel.style.filter ='hue-rotate(${hue}deg';
+    }
+});
